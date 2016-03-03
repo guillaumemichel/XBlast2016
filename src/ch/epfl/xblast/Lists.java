88bf601cfd245d220760e7b2b790dfec4b6bb43b
gmichel <guillaume.michel@epfl.ch>
2016-03-03 23:52:51 +0100

    Question

--- src/ch/epfl/xblast/Lists.java
+++ src/ch/epfl/xblast/Lists.java
@@ -31,7 +31,7 @@ public final class Lists {
         if(l.isEmpty()){
             throw new IllegalArgumentException();
         }else{
-            List<T> subList=l.subList(0, l.size());
+            List<T> subList=l.subList(0, l.size()); //l.size()-2 ???
             Collections.reverse(subList);
             l.addAll(subList);
             return l;
