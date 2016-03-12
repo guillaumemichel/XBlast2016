6ad37864f6f7d793008fe78092c4da7ed0fb0326
Guillaume <guillaume.michel@epfl.ch>
2016-03-12 22:59:28 +0100

    More changes about Lists.ajav

--- src/ch/epfl/xblast/Lists.java
+++ src/ch/epfl/xblast/Lists.java
@@ -53,35 +53,54 @@ public final class Lists {
             l1.add(l2);
             return l1;
         }
-        return recursivePermutations(l);
+        return permutationsBis(l);
     }
     
-    public static <T> List<List<T>> recursivePermutations(List<T> l){
+    public static <T> List<List<T>> permutationsBis(List<T> l){
         ArrayDeque<T> copy = new ArrayDeque<T>(l);
         ArrayDeque<T> el = new ArrayDeque<T>();
         ArrayDeque<T> temp;
-        System.out.println(l);
-        ArrayList<ArrayList<T>> list = new ArrayList<>();
+        List<List<T>> list = new ArrayList<>();
         while (copy.size()>2){
             el.addFirst(copy.getFirst());
             copy.removeFirst();
-        }System.out.println(el);
+        }System.out.println(el+" el");
         while (!el.isEmpty()){
-            temp = new ArrayDeque<>(copy);
             for (int i=0;i<copy.size();++i){
+                temp = new ArrayDeque<>(copy);
                 for (int j=0;j<i;++j){
                     temp.addFirst(temp.getLast());
                     temp.removeLast();
                 }
                 temp.addFirst(el.getFirst());
-                el.removeFirst();
-                for(int j=0;j<copy.size()+1-i;++j){
+                list.add(new ArrayList<T>(temp));
+                for (int j=0;j<temp.size()-1;++j){
                     temp.addFirst(temp.getLast());
                     temp.removeLast();
+                    list.add(new ArrayList<T>(temp));
                 }
-                list.add(new ArrayList<T>(temp));
             }
+            el.removeFirst();
         }
-        return null;
+        return list;
+    }
+    
+    public static <T> List<List<T>> recursionLists(List<List<T>> l,T el){
+        ArrayList<List<T>> copy = new ArrayList<>(l);
+        List<T> temp = new ArrayList<>();
+        ArrayList<List<T>> list = new ArrayList<>();
+        for (int i=0;i<copy.size();++i){
+            for (int j=0;(j<i)&&(j<copy.get(i).size());++j){
+                temp.add(copy.get(i).get(j));
+            }
+            temp.add(el);
+            for (int j=0;j+i<copy.get(i).size();++i){
+                temp.add(copy.get(i).get(i+j));
+            }
+            list.add(temp);
+            temp.clear();
+        }
+        return list;
+        
     }
 }
