ac00b1498e5ef4fba331e3c3bd6f2e7b061a9912
Guillaume <guillaume.michel@epfl.ch>
2016-03-12 17:11:07 +0100

    Lists in process

--- src/ch/epfl/xblast/Lists.java
+++ src/ch/epfl/xblast/Lists.java
@@ -1,5 +1,6 @@
 package ch.epfl.xblast;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -44,18 +45,43 @@ public final class Lists {
             l1.add(l);
             return l1;
         }
-        return null;
+        if (l.size()==2){
+            l1.add(l);
+            List<T> l2 = new ArrayList<>();
+            l2.add(l.get(1));
+            l2.add(l.get(0));
+            l1.add(l2);
+            return l1;
+        }
+        return recursivePermutations(l);
     }
     
     public static <T> List<List<T>> recursivePermutations(List<T> l){
-        ArrayList<T> copy = new ArrayList<T>(l);
-        ArrayList<T> el = new ArrayList<T>();
+        ArrayDeque<T> copy = new ArrayDeque<T>(l);
+        ArrayDeque<T> el = new ArrayDeque<T>();
+        ArrayDeque<T> temp;
+        System.out.println(l);
         ArrayList<ArrayList<T>> list = new ArrayList<>();
         while (copy.size()>2){
-            el.add(copy.get(0));
-            copy.remove(0);
+            el.addFirst(copy.getFirst());
+            copy.removeFirst();
+        }System.out.println(el);
+        while (!el.isEmpty()){
+            temp = new ArrayDeque<>(copy);
+            for (int i=0;i<copy.size();++i){
+                for (int j=0;j<i;++j){
+                    temp.addFirst(temp.getLast());
+                    temp.removeLast();
+                }
+                temp.addFirst(el.getFirst());
+                el.removeFirst();
+                for(int j=0;j<copy.size()+1-i;++j){
+                    temp.addFirst(temp.getLast());
+                    temp.removeLast();
+                }
+                list.add(new ArrayList<T>(temp));
+            }
         }
-        while ()
         return null;
     }
 }
