314deb2c358750a6ee4cedb403ef32883acb0083
Adrien Vandenbroucque <adrien.vandenbroucque@epfl.ch>
2016-03-13 22:18:29 +0100

    Juste syntaxe de l'opérateur ternaire (ajout de parenthèses)

--- src/ch/epfl/xblast/server/Bonus.java
+++ src/ch/epfl/xblast/server/Bonus.java
@@ -12,14 +12,14 @@ public enum Bonus {
     INC_BOMB {
       @Override
       public Player applyTo(Player player) {
-          return player.maxBombs()<9 ? player.withMaxBombs(player.maxBombs()+1):player;
+          return (player.maxBombs()<9) ? player.withMaxBombs(player.maxBombs()+1) : player;
       }
     },
 
     INC_RANGE {
       @Override
       public Player applyTo(Player player) { 
-          return player.bombRange()<9 ? player.withBombRange(player.bombRange()+1):player;
+          return (player.bombRange()<9) ? player.withBombRange(player.bombRange()+1) : player;
       }
     };
 
