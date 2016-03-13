79dd658aeb65294920780aaec631f36badcb1f5d
Guillaume <guillaume.michel@epfl.ch>
2016-03-13 12:55:35 +0100

    modified permutations() to run on my war machine to compare

--- src/ch/epfl/xblast/Lists.java
+++ src/ch/epfl/xblast/Lists.java
@@ -2,6 +2,7 @@ package ch.epfl.xblast;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -39,13 +40,35 @@ public final class Lists {
         }
     }
     
-    /**
-     * Return a list of all possible combinations 
-     * @param l
-     * @return
-     */
-    
     public static <T> List<List<T>> permutations(List<T> l){
+        List<T> copy = new ArrayList<>(l);
+        List<List<T>> list = new ArrayList<>();
+        int s1 = copy.size();
+        if (s1<2){
+            list.add(copy);
+            return list;
+        }
+        if (s1==2){
+            list.add(new ArrayList<>(copy));
+            Collections.reverse(copy);
+            list.add(new ArrayList<>(copy));
+            return list;
+        }
+        List<List<T>> l1 = new ArrayList<>();
+        l1.addAll(permutations(copy.subList(1, s1)));
+        List<T> temp;
+        T el = copy.get(0);
+        for (List<T> t:l1){
+            for (int i=0;i<s1;++i){
+                temp= new ArrayList<>(t);
+                temp.add(i,el);
+                list.add(temp);
+            }
+        }
+        return list;
+    }
+    
+    public static <T> List<List<T>> permutationsBis(List<T> l){
         ArrayList<T> copy = new ArrayList<T>(l);
         ArrayDeque<T> el = new ArrayDeque<T>();
         ArrayList<T> temp = new ArrayList<>();
---
+++ tests/ch/epfl/xblast/ListsPermutationsTest.java
@@ -0,0 +1,33 @@
+package ch.epfl.xblast;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class ListsPermutationsTest {
+
+    @Test
+    public void test() {
+        List<Integer> l = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8));
+        List<List<Integer>> check = new ArrayList<>();
+        int factorial=1;
+        List<List<Integer>> t = Lists.permutationsBis(l);
+        
+        for(int i=2; i<=l.size(); i++){
+            factorial*=i;
+        }
+        
+        for(List<Integer> e:t){
+            if(!check.contains(e)){
+                check.add(e);
+            }
+        }
+        
+        assertEquals(check.size(), factorial);
+    }
+
+}
