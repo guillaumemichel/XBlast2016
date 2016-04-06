d7838df32f4d8e51690dfdad974ea7aa1d6fbe50
Guillaume <guillaume.michel@epfl.ch>
2016-04-06 13:03:54 +0200

    GameTest

--- src/ch/epfl/xblast/server/debug/GameTest.java
+++ src/ch/epfl/xblast/server/debug/GameTest.java
@@ -31,45 +31,38 @@ public class GameTest {
         Block __ = Block.FREE;
         Block XX = Block.INDESTRUCTIBLE_WALL;
         Block xx = Block.DESTRUCTIBLE_WALL;
+        Block br = Block.BONUS_RANGE;
         Board board = Board.ofQuadrantNWBlocksWalled(
           Arrays.asList(
-            Arrays.asList(__, __, __, __, __, __, __),
+            Arrays.asList(br, br, br, br, br, br, br),
             Arrays.asList(__, XX, xx, XX, xx, XX, xx),
             Arrays.asList(__, xx, __, __, __, xx, __),
-            Arrays.asList(xx, XX, __, XX, XX, XX, XX),
+            Arrays.asList(__, XX, __, XX, XX, XX, XX),
             Arrays.asList(__, xx, __, xx, __, __, __),
-            Arrays.asList(xx, XX, xx, XX, xx, XX, __)));
+            Arrays.asList(__, __, __, __, __, __, __)));
         
         List<Player> players = new ArrayList<>();
-        players.add(new Player(PlayerID.PLAYER_1,3, new Cell(1, 1), 2, 3));
-        players.add(new Player(PlayerID.PLAYER_2, 3, new Cell(13, 1), 2, 3));
-        players.add(new Player(PlayerID.PLAYER_3, 3, new Cell(13, 11), 2, 3));
-        players.add(new Player(PlayerID.PLAYER_4, 3, new Cell(1, 11), 2, 3));
-        
-        Set<PlayerID> set=new HashSet<>();
-        set.add(PlayerID.PLAYER_4);
-        
-        Map<PlayerID, Optional<Direction>> map=new HashMap<>();
-        map.put(PlayerID.PLAYER_1, Optional.of(Direction.E));
+        players.add(new Player(PlayerID.PLAYER_1,3, new Cell(1, 1), 2, 1));
+        players.add(new Player(PlayerID.PLAYER_2,1, new Cell(10, 10), 2, 1));
+        players.add(new Player(PlayerID.PLAYER_3,0, new Cell(13, 13), 2, 1));
+        players.add(new Player(PlayerID.PLAYER_4,0, new Cell(13, 12), 2, 1));
 
         GameState g = new GameState(board, players);
         GameStatePrinter.printGameState(g);
 
-        while(! g.isGameOver()){
-            g=g.next(randomEvents.randomSpeedChangeEvents(), randomEvents.randomBombDropEvents());
-            GameStatePrinter.printGameState(g);
-            Thread.sleep(50);
-            //System.out.print("\u001b[2J");
-            System.out.println();
-        }
         int t=0;
-        while (!g.isGameOver()){
-            
+        Map<PlayerID,Optional<Direction>> map1 = new HashMap<>();
+        map1.put(PlayerID.PLAYER_1,Optional.of(Direction.E));
+        while (t<1000){
+            if (t==0)
+                g = g.next(map1, new HashSet<>());
+            else
+                g = g.next(new HashMap<>(), new HashSet<>());
             GameStatePrinter.printGameState(g);
             Thread.sleep(50);
             System.out.println();
+            ++t;
         }
-        
     }
 
 }
