6e67a4eed508db8debf9f8f9eb5eba570ea2e2f0
Guillaume <guillaume.michel@epfl.ch>
2016-04-19 17:01:05 +0200

    serializing

--- src/ch/epfl/xblast/server/painter/GameStateSerializer.java
+++ src/ch/epfl/xblast/server/painter/GameStateSerializer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ch.epfl.xblast.Cell;
+import ch.epfl.xblast.Direction;
 import ch.epfl.xblast.server.GameState;
 import ch.epfl.xblast.server.Player;
 
@@ -13,11 +14,23 @@ public final class GameStateSerializer {
     
     public static List<Byte> serialize(BoardPainter p, GameState g){
         List<Byte> list = new ArrayList<>();
+        List<Byte> temp = new ArrayList<>();
         for (Cell c : Cell.SPIRAL_ORDER){
-            list.add(p.byteForCell(g.board(),c));
+            temp.add(p.byteForCell(g.board(),c));
         }
-        //compress
-        //sérialisation des bombes et explosions
+        list.addAll()
+        for (Cell c : Cell.ROW_MAJOR_ORDER){
+            if (g.board().blockAt(c).isFree()){
+                if (g.blastedCells().contains(c))
+                    list.add(ExplosionPainter.byteForBlast(g.blastedCells().contains(c.neighbor(Direction.N)), g.blastedCells().contains(c.neighbor(Direction.E)), g.blastedCells().contains(c.neighbor(Direction.S)), g.blastedCells().contains(c.neighbor(Direction.W))));
+                
+                else if (g.bombedCells().containsKey(c))
+                    list.add(ExplosionPainter.byteForBomb(g.bombedCells().get(c)));
+                
+                else list.add(ExplosionPainter.BYTE_FOR_EMPTY);
+            }
+            else list.add(ExplosionPainter.BYTE_FOR_EMPTY);
+        }
         for (Player player : g.players()){
             list.add((byte) player.lives());
             list.add((byte) player.position().x());
@@ -25,6 +38,6 @@ public final class GameStateSerializer {
             list.add(PlayerPainter.byteForPlayer(g.ticks(), player));
         }
         list.add((byte) (2*Math.ceil(g.remainingTime()/4)));
-        return null;
+        return list;
     }
 }
