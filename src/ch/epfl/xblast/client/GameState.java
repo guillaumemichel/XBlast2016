52cdad1bfcc9c425e808bfd1609dc9ddfa04a3f9
Guillaume <guillaume.michel@epfl.ch>
2016-04-25 14:44:27 +0200

    osdgonsvdoinsf

--- src/ch/epfl/xblast/client/GameState.java
+++ src/ch/epfl/xblast/client/GameState.java
@@ -1,5 +1,15 @@
 package ch.epfl.xblast.client;
 
+import java.awt.Image;
+import java.util.List;
+
 public final class GameState {
 
+    public GameState(List<Player> imagePlayer, List<Image> imageExplosion, List<Image> image){
+        
+    }
+    
+    public final static class Player{
+        
+    }
 }
--- src/ch/epfl/xblast/client/GameStateDeserializer.java
+++ src/ch/epfl/xblast/client/GameStateDeserializer.java
@@ -1,8 +1,11 @@
 package ch.epfl.xblast.client;
 
 import java.awt.Image;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import ch.epfl.xblast.RunLengthEncoder;
 
@@ -10,29 +13,27 @@ public final class GameStateDeserializer {
     
     private GameStateDeserializer(){};
     
-    public static GameState deserializeGameState(List<Byte> l){
+    public static GameState deserializeGameState(List<Byte> l) throws NoSuchElementException, URISyntaxException, IOException{
         List<Byte> fullBoard;
         List<Byte> fullExplosion;
         List<Image> boardImage=new ArrayList<>();
         List<Image> explosionImage=new ArrayList<>();
-        //deserialize board
-        int bs = l.get(0); //board size
-        int es = l.get(bs); //explosion size
         
-        ImageCollection c1 = new ImageCollection("board"); // à définir
-        fullBoard = RunLengthEncoder.decode(l.subList(0, bs-1));
+        int bs = l.get(0); //board size
+        int es = l.get(bs+1); //explosion size
+        
+        ImageCollection c1 = new ImageCollection("block");
+        fullBoard = RunLengthEncoder.decode(l.subList(1, bs+1));
         for (Byte b : fullBoard){
             boardImage.add(c1.image(b));
         }
         
         ImageCollection c2 = new ImageCollection("explosion");
+        fullExplosion = RunLengthEncoder.decode(l.subList(bs+2, bs+es+2));
         for (Byte b : fullExplosion){
             explosionImage.add(c2.image(b));
         }
         
-        
-        fullExplosion = RunLengthEncoder.decode(l.subList(bs, bs+es-1));
-        
-        return null;
+        return new GameState();
     }
 }
