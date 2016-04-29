3d7597e7459157822ef188e0c19cab7ed67dbeb0
Adrien Vandenbroucque <adrien.vandenbroucque@epfl.ch>
2016-04-29 13:44:45 +0200

    imageCollection done

--- src/ch/epfl/xblast/client/ImageCollection.java
+++ src/ch/epfl/xblast/client/ImageCollection.java
@@ -4,11 +4,11 @@ import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.TreeMap;
 
 import javax.imageio.ImageIO;
 
@@ -22,31 +22,32 @@ import javax.imageio.ImageIO;
 public final class ImageCollection {
     private final String dirName;
     
-    private final List<Image> imagesOfDir;
+    private final Map<Byte, Image> imagesOfDir;
     
     public ImageCollection(String dirName) throws NullPointerException{
         this.dirName=dirName;
-        imagesOfDir = Collections.unmodifiableList(Objects.requireNonNull(loadImages(this.dirName)));
+        imagesOfDir = Collections.unmodifiableMap(Objects.requireNonNull(loadImages()));
     }
     
-    private static List<Image> loadImages(String dirName){
-        List<Image> images = new ArrayList<>();
-        File dir;
+    private Map<Byte, Image> loadImages(){
+        Map<Byte, Image> images = new TreeMap<>();
+        File dir=null;
         
         try{
             dir=new File(ImageCollection.class
                     .getClassLoader()
-                    .getResource(dirName)
+                    .getResource(this.dirName)
                     .toURI());
         }catch(URISyntaxException e){
-            dir=null;
-            throw new NullPointerException("Invalid diectory name !");
+
         }
         
         for (File file : dir.listFiles()) {
             try{
-                images.add(ImageIO.read(file));
+                images.put((byte)Integer.parseInt(file.getName().substring(0, 3)), ImageIO.read(file));
             }catch(IOException e){
+                
+            }catch(NumberFormatException e){
                 
             }
         }  
@@ -54,7 +55,10 @@ public final class ImageCollection {
     }
     
     public Image imageOrNull(int index){
-        return imagesOfDir.get(index);
+        if(imagesOfDir.containsKey((byte) index))
+            return imagesOfDir.get((byte)index);
+        
+        return null;
     }
     
     public Image image(int index) throws NoSuchElementException{
