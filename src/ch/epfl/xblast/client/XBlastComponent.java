dc6f541526502eff4678894cce482f9f16fd37a8
Adrien Vandenbroucque <adrien.vandenbroucque@epfl.ch>
2016-05-02 18:01:16 +0200

    d

--- src/ch/epfl/xblast/client/XBlastComponent.java
+++ src/ch/epfl/xblast/client/XBlastComponent.java
@@ -2,12 +2,16 @@ package ch.epfl.xblast.client;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.image.ImageObserver;
 
 import javax.swing.JComponent;
 
+import ch.epfl.xblast.PlayerID;
+
 public final class XBlastComponent extends JComponent{
+    private GameState gameState = null;
+    private PlayerID id = null;
     
     @Override
     public Dimension getPreferredSize(){
@@ -15,11 +19,22 @@ public final class XBlastComponent extends JComponent{
     }
     
     @Override
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g0){
+        Graphics2D g = (Graphics2D)g0;
+        
+        int widthOfBlock = gameState.imagesBoard().get(0).getWidth(null);
+        int heightOfBlock = gameState.imagesBoard().get(0).getHeight(null);
+
+        for(int i = 0; i < gameState.imagesBoard().size(); ++i){
+            
+        }
         
     }
     
-    private boolean drawImage(Image img, int x, int y, ImageObserver observer){
-        
+    private void setGameState(GameState gameState, PlayerID id){
+        this.gameState = gameState;
+        this.id = id;
+        repaint();
     }
+    
 }
