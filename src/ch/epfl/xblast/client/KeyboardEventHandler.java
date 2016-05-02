c817e3d80e2c84668a9e8369e4e929964d6b6583
Guillaume <guillaume.michel@epfl.ch>
2016-05-02 18:36:48 +0200

    f

--- src/ch/epfl/xblast/client/KeyboardEventHandler.java
+++ src/ch/epfl/xblast/client/KeyboardEventHandler.java
@@ -2,10 +2,31 @@ package ch.epfl.xblast.client;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyListener;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Consumer;
 
+import ch.epfl.xblast.PlayerAction;
+
+/**
+ * A keyboard event handler
+ * 
+ * @author Guilaume Michel (258066)
+ * @author Adrien Vandenbroucque (258715)
+ *
+ */
 public class KeyboardEventHandler extends KeyAdapter implements KeyListener{
     
-    public KeyboardEventHandler(){
+    Map<Integer,PlayerAction> keymap;
+    Consumer c;
+    
+    public KeyboardEventHandler(Map<Integer,PlayerAction> map, Consumer<PlayerAction> cons){
+        keymap = Collections.unmodifiableMap(map);
+        c=cons;
+    }
+    
+    @Override
+    public void keyPressed(){
         
     }
 }
