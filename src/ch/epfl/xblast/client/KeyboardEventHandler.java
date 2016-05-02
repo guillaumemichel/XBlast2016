c9fe0f1862497c5487a8351b4cdb1b13a421157e
gmichel <guillaume.michel@epfl.ch>
2016-05-02 19:52:32 +0200

    keyboard truc

--- src/ch/epfl/xblast/client/KeyboardEventHandler.java
+++ src/ch/epfl/xblast/client/KeyboardEventHandler.java
@@ -1,6 +1,7 @@
 package ch.epfl.xblast.client;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Collections;
 import java.util.Map;
@@ -18,15 +19,25 @@ import ch.epfl.xblast.PlayerAction;
 public class KeyboardEventHandler extends KeyAdapter implements KeyListener{
     
     Map<Integer,PlayerAction> keymap;
-    Consumer c;
+    Consumer<PlayerAction> c;
     
+    /**
+     * Build a keyboard event handler from a map and a consumer
+     * 
+     * @param map
+     * 			The map that contains the meaning of each key/command on the keyboard
+     * @param cons
+     * 			The consumer
+     */
     public KeyboardEventHandler(Map<Integer,PlayerAction> map, Consumer<PlayerAction> cons){
         keymap = Collections.unmodifiableMap(map);
         c=cons;
     }
     
     @Override
-    public void keyPressed(){
-        
+    public void keyPressed(KeyEvent k){
+        if (keymap.containsKey(k.getKeyCode())){
+        	c.accept(keymap.get(k.getKeyCode()));
+        }
     }
 }
