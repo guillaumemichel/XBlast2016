6eb791fc88b9e234ad2c2cdda45ff9dcd312af0d
Guillaume <guillaume.michel@epfl.ch>
2016-05-02 15:37:38 +0200

    keyboardjg bijbeijef

---
+++ src/ch/epfl/xblast/client/KeyboardEventHandler.java
@@ -0,0 +1,11 @@
+package ch.epfl.xblast.client;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyListener;
+
+public class KeyboardEventHandler extends KeyAdapter implements KeyListener{
+    
+    public KeyboardEventHandler(){
+        
+    }
+}
