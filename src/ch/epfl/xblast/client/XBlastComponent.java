bfcb047b8a515f32de6205bc65e0fb21f26b237e
Adrien Vandenbroucque <adrien.vandenbroucque@epfl.ch>
2016-05-02 15:45:13 +0200

    a

--- src/ch/epfl/xblast/client/XBlastComponent.java
+++ src/ch/epfl/xblast/client/XBlastComponent.java
@@ -2,6 +2,8 @@ package ch.epfl.xblast.client;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
 
 import javax.swing.JComponent;
 
@@ -14,6 +16,10 @@ public final class XBlastComponent extends JComponent{
     
     @Override
     public void paintComponent(Graphics g){
+        
+    }
+    
+    private boolean drawImage(Image img, int x, int y, ImageObserver observer){
         
     }
 }
