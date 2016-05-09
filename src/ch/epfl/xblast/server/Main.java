40a65d668a9133538e6fb93a556d6d3ae16e18fa
Adrien Vandenbroucque <adrien.vandenbroucque@epfl.ch>
2016-05-09 17:58:33 +0200

    server

--- src/ch/epfl/xblast/server/Main.java
+++ src/ch/epfl/xblast/server/Main.java
@@ -7,41 +7,66 @@ import java.net.StandardProtocolFamily;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import ch.epfl.xblast.PlayerAction;
 import ch.epfl.xblast.PlayerID;
+import ch.epfl.xblast.server.painter.BoardPainter;
 
 public final class Main {
 
     public static void main(String[] args) {
         int numberOfPlayers = GameState.PLAYER_NUMBER;
-        int currentPlayerID = 0;
-        Map<SocketAddress, PlayerID> players = new HashMap<>();
                     
         if(args.length==1)
             numberOfPlayers = Integer.parseInt(args[0]);
         
-        try{
+        try {
             DatagramChannel channel = DatagramChannel.open(StandardProtocolFamily.INET);
             channel.bind(new InetSocketAddress(2016));
             
-            ByteBuffer buffer = ByteBuffer.allocate(1);
-            SocketAddress senderAddress;
+            Map<SocketAddress, PlayerID> players = joiningGame(numberOfPlayers, channel);
             
-            while(players.size()!= numberOfPlayers){
-                senderAddress = channel.receive(buffer);
-                if(buffer.get(0)== PlayerAction.JOIN_GAME.ordinal()){
-                    players.put(senderAddress, PlayerID.values()[currentPlayerID]);
-                    ++currentPlayerID;
-                }
-            }
+            long currentTime = System.nanoTime();
+            
+            GameState g = Level.DEFAULT_LEVEL.gameState();
+            BoardPainter b = Level.DEFAULT_LEVEL.boardPainter();
+            
+            while(!g.isGameOver()){
                 
-            
-
-        }catch(IOException e){
-            
+                List<Byte> serializedGameState = GameStateSerializer.serialize(b, g);
+                ByteBuffer buffer = ByteBuffer.allocate(1);
+                for(Byte bytes : serializedGameState)
+                    buffer.put(bytes);
+                buffer.flip();
+                  
+                for(SocketAddress address : players.keySet())
+                    channel.send(buffer, address);
+                
+                currentTime+=Ticks.TICK_NANOSECOND_DURATION;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+    }
+    
+    private static Map<SocketAddress, PlayerID> joiningGame(int numberOfplayers, DatagramChannel channel) throws IOException {
+        Map<SocketAddress, PlayerID> players = new HashMap<>();
+        int currentPlayerID = 0;
+        
+        ByteBuffer buffer = ByteBuffer.allocate(1);
+        SocketAddress senderAddress;
+        
+        while(players.size()!= numberOfplayers){
+            senderAddress = channel.receive(buffer);
+            if(buffer.get(0)== PlayerAction.JOIN_GAME.ordinal()){
+                players.put(senderAddress, PlayerID.values()[currentPlayerID]);
+                ++currentPlayerID;
+            }
+        }
+        return players;        
     }
 
 }
