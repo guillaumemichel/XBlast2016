ed9062f97d0de100e64b47d6403d90db8f590fa4
Guillaume <guillaume.michel@epfl.ch>
2016-05-10 14:20:41 +0200

    infwinfw+

--- src/ch/epfl/xblast/client/Main.java
+++ src/ch/epfl/xblast/client/Main.java
@@ -4,18 +4,50 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.StandardProtocolFamily;
+import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.epfl.xblast.PlayerAction;
+import ch.epfl.xblast.PlayerID;
 
 public final class Main {
 
     public static void main(String[] args) {
         try {
-            DatagramChannel channel =
-                    DatagramChannel.open(StandardProtocolFamily.INET);
+            DatagramChannel channel = DatagramChannel.open(StandardProtocolFamily.INET);
+            SocketAddress chaussette = new InetSocketAddress(args.length==0 ? "localhost": args[0],2016);
+            channel.bind(chaussette).configureBlocking(false);
+            
+            ByteBuffer b = joinGame(channel, chaussette);
+            PlayerID id = PlayerID.values()[b.get(0)];
+            List<Byte> firstState = new ArrayList<>();
+            
+            while (b.hasRemaining()){
+                
+            }
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
-        SocketAddress chaussette = new InetSocketAddress(args.length==0 ? args[0]:"localhost",2016);
+        
+    }
+    
+    private static ByteBuffer joinGame(DatagramChannel channel,SocketAddress chaussette) {
+        ByteBuffer join = ByteBuffer.allocate(1);
+        ByteBuffer firstState = ByteBuffer.allocate(410);
+        
+        join.put((byte)PlayerAction.JOIN_GAME.ordinal()).flip();
+        try {
+            do {
+                channel.send(join, chaussette);
+                channel.receive(firstState);
+            } while (!firstState.hasRemaining());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return firstState;
     }
 
 }
