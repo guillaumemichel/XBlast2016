984a27f58498888706382d2358e6d813de0e77af
Adrien Vandenbroucque <adrien.vandenbroucque@epfl.ch>
2016-05-30 14:02:52 +0200

    S

--- src/ch/epfl/xblast/server/mapEditor/GridOfBlocks.java
+++ src/ch/epfl/xblast/server/mapEditor/GridOfBlocks.java
@@ -2,7 +2,6 @@ package ch.epfl.xblast.server.mapEditor;
 
 import java.awt.GridLayout;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
