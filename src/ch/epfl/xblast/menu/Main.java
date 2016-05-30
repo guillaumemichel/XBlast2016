d1583e7449264f7109bca378f462f9e728379648
Guillaume <guillaume.michel@epfl.ch>
2016-05-30 23:38:33 +0200

    refuck

--- src/ch/epfl/xblast/menu/Main.java
+++ src/ch/epfl/xblast/menu/Main.java
@@ -3,16 +3,9 @@ package ch.epfl.xblast.menu;
 import javax.swing.SwingUtilities;
 
 public final class Main {
-    /*public static void main(String[] args) {
-        ModelMenu model = new ModelMenu();
-        ViewMenu view = new ViewMenu(model);
-        SwingUtilities.invokeLater(()->{
-            ControllerMenu c = new ControllerMenu(view,model);
-        });
-    }*/
     public static void main(String[] args){
         View v = new View();
         Model m = new Model(v);
-        Controller c = new Controller(v,m);
+        SwingUtilities.invokeLater(() -> new Controller(v,m));
     }
 }
